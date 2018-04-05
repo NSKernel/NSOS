313e9a44c4db3bcca2737644e71728d81e064147
NSKernel <shixuan.zhao@hotmail.com>
2018-04-05 18:47:46 +0800

    Added support for capslock and shift combined keys

--- include/os/console.h
+++ include/os/console.h
@@ -3,7 +3,12 @@
 #define _OS_CONSOLE_H_
 
 void initconsole(_Device *dev);
-void printchar(char ch, int x, int y);
+void setcursor(unsigned int x, unsigned int y, char on);
+unsigned int getcursorx();
+unsigned int getcursory();
+char getcursoron();
+void printcharpos(char ch, int x, int y);
+void printchar(char ch);
 void printstring(char *buf);
 
 #endif
