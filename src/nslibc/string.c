82ef4c368300d1ac97eafc41416a39484f0eba5e
tracer <tracer@tracer.org>
2018-04-05 12:08:21 +0800

    > build nskernel Linux NSUBUNTU0 4.13.0-38-generic #43~16.04.1-Ubuntu SMP Wed Mar 14 17:46:42 UTC 2018 i686 i686 i686 GNU/Linux

--- src/nslibc/string.c
+++ src/nslibc/string.c
@@ -32,7 +32,7 @@ char *strcpy(char *dest, const char *src) {
     return dest;
 }
 
-char *strncpy(char *dst, const char *src, size_t n) {
+char *strncpy(char *dest, const char *src, size_t n) {
     size_t i;
     for (i = 0; i < n && src[i] != '\0'; i++)
         dest[i] = src[i];
