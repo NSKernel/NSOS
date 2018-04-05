8bc6b6bc958eb954b737c7d75e873d591724240d
tracer <tracer@tracer.org>
2018-04-05 15:20:31 +0800

    > run nskernel Linux NSUBUNTU0 4.13.0-38-generic #43~16.04.1-Ubuntu SMP Wed Mar 14 17:46:42 UTC 2018 i686 i686 i686 GNU/Linux

--- src/main.c
+++ src/main.c
@@ -47,7 +47,7 @@ int main() {
   initconsole(_device(deviceit));
   printstring("Test\n");
   printstring("abcdefghijklmnopqrstuvwxyz12345678901234567890abcdefghijklmnopqrstuvwxyz12345678901234567890");
-  while (count < 25) {
+  while (count < 40) {
       sprintf(testbuf, "Hello the %d times.\n", count);
       count += 1;
       printstring(testbuf); 
